2811e2521a1ef150863fc159f42e1410c565db1f
Ikshit <ikshitmagoon99@gmail.com>
2021-06-11 15:18:45 +0530

    Models Added

---
+++ Models/Customers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace UrbanMediMart.Models
+{
+    public partial class Customers
+    {
+        public Customers()
+        {
+            Orders = new HashSet<Orders>();
+        }
+
+        public int CustomerId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string PhoneNumber { get; set; }
+        public string ZipCode { get; set; }
+        public DateTime RegistrationDate { get; set; }
+
+        public virtual ICollection<Orders> Orders { get; set; }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace UrbanMediMart.Models
+{
+    public partial class Product
+    {
+        public int MedicineId { get; set; }
+        public string MedicineName { get; set; }
+        public string Company { get; set; }
+        public double Price { get; set; }
+        public double? Discount { get; set; }
+        public DateTime MfgDate { get; set; }
+        public DateTime ExpDate { get; set; }
+        public double Units { get; set; }
+        public byte[] Pimage { get; set; }
+        public double? CategoryId { get; set; }
+
+        public virtual Category Category { get; set; }
+    }
+}
