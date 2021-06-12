c54f08ef018f810f93e76ce2e27647b3440a3256
Ikshit <ikshitmagoon99@gmail.com>
2021-06-12 15:37:47 +0530

    final-commit

--- Models/UrbanMediMartContext.cs
+++ Models/UrbanMediMartContext.cs
@@ -31,15 +31,9 @@ namespace UrbanMediMart.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-<<<<<<< HEAD
-<<<<<<< HEAD
+
                 optionsBuilder.UseSqlServer("Server=DESKTOP-LQFLIB6\\IKSHIT;Database=UrbanMediMart;Trusted_Connection=true;");
-=======
-                optionsBuilder.UseSqlServer("Server=DESKTOP-UENS42J\\SQLEXPRESS;database=UrbanMediMart;trusted_connection=true");
->>>>>>> yaman
-=======
-                optionsBuilder.UseSqlServer("Server=LAPTOP-5TQNB10G\\SQLEXPRESS;Database=UrbanMediMart;Trusted_Connection=True;");
->>>>>>> mohitB
+
             }
         }
 
@@ -48,28 +42,15 @@ namespace UrbanMediMart.Models
             modelBuilder.Entity<Admin>(entity =>
             {
                 entity.HasIndex(e => e.Email)
-<<<<<<< HEAD
-<<<<<<< HEAD
+
                     .HasName("UQ__Admin__AB6E6164F4EBA9DB")
                     .IsUnique();
 
                 entity.HasIndex(e => e.FullName)
                     .HasName("UQ__Admin__19491390174AEE63")
-=======
-                    .HasName("UQ__Admin__AB6E61649F08D924")
                     .IsUnique();
 
-                entity.HasIndex(e => e.FullName)
-                    .HasName("UQ__Admin__19491390C8A17CE4")
->>>>>>> yaman
-=======
-                    .HasName("UQ__Admin__AB6E6164E741286F")
-                    .IsUnique();
-
-                entity.HasIndex(e => e.FullName)
-                    .HasName("UQ__Admin__194913902B16B8E3")
->>>>>>> mohitB
-                    .IsUnique();
+             
 
                 entity.Property(e => e.Id).ValueGeneratedNever();
 
@@ -100,24 +81,12 @@ namespace UrbanMediMart.Models
             modelBuilder.Entity<Customers>(entity =>
             {
                 entity.HasKey(e => e.CustomerId)
-<<<<<<< HEAD
-<<<<<<< HEAD
+
                     .HasName("PK__Customer__A4AE64B813CC1052");
 
                 entity.HasIndex(e => e.Email)
                     .HasName("UQ__Customer__A9D10534BBB184D6")
-=======
-                    .HasName("PK__Customer__A4AE64B8EF6580D1");
 
-                entity.HasIndex(e => e.Email)
-                    .HasName("UQ__Customer__A9D105343AE287BB")
->>>>>>> yaman
-=======
-                    .HasName("PK__Customer__A4AE64B822F6A905");
-
-                entity.HasIndex(e => e.Email)
-                    .HasName("UQ__Customer__A9D10534475CFAAE")
->>>>>>> mohitB
                     .IsUnique();
 
                 entity.Property(e => e.CustomerId)
@@ -174,50 +143,23 @@ namespace UrbanMediMart.Models
                 entity.HasOne(d => d.Order)
                     .WithMany(p => p.OrderDetail)
                     .HasForeignKey(d => d.OrderId)
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
+
                     .HasConstraintName("FK__Order_det__Order__6477ECF3");
-=======
-                    .HasConstraintName("FK__Order_det__Order__5CD6CB2B");
->>>>>>> itika
-=======
-                    .HasConstraintName("FK__Order_det__Order__48CFD27E");
->>>>>>> yaman
-=======
-                    .HasConstraintName("FK__Order_det__Order__48CFD27E");
->>>>>>> mohitB
 
                 entity.HasOne(d => d.Product)
                     .WithMany(p => p.OrderDetail)
                     .HasForeignKey(d => d.ProductId)
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
+
                     .HasConstraintName("FK__Order_det__Produ__656C112C");
-=======
-                    .HasConstraintName("FK__Order_det__Produ__5DCAEF64");
->>>>>>> itika
-=======
-                    .HasConstraintName("FK__Order_det__Produ__49C3F6B7");
->>>>>>> yaman
-=======
-                    .HasConstraintName("FK__Order_det__Produ__49C3F6B7");
->>>>>>> mohitB
+
             });
 
             modelBuilder.Entity<Orders>(entity =>
             {
                 entity.HasKey(e => e.OrderId)
-<<<<<<< HEAD
-<<<<<<< HEAD
+
                     .HasName("PK__Orders__465962294DA16B87");
-=======
-                    .HasName("PK__Orders__46596229F65BC608");
->>>>>>> yaman
-=======
-                    .HasName("PK__Orders__46596229FF047197");
->>>>>>> mohitB
+
 
                 entity.Property(e => e.OrderId)
                     .HasColumnName("order_id")
@@ -248,29 +190,17 @@ namespace UrbanMediMart.Models
                 entity.HasOne(d => d.Customer)
                     .WithMany(p => p.Orders)
                     .HasForeignKey(d => d.CustomerId)
-<<<<<<< HEAD
-<<<<<<< HEAD
+
                     .HasConstraintName("FK__Orders__Customer__3D5E1FD2");
-=======
-                    .HasConstraintName("FK__Orders__Customer__44FF419A");
->>>>>>> yaman
-=======
-                    .HasConstraintName("FK__Orders__Customer__44FF419A");
->>>>>>> mohitB
+
             });
 
             modelBuilder.Entity<Product>(entity =>
             {
                 entity.HasKey(e => e.MedicineId)
-<<<<<<< HEAD
-<<<<<<< HEAD
+
                     .HasName("PK__Product__4F2128F04A5010DD");
-=======
-                    .HasName("PK__Product__4F2128F0B2FE0103");
->>>>>>> yaman
-=======
-                    .HasName("PK__Product__4F2128F02F9F9FFE");
->>>>>>> mohitB
+
 
                 entity.Property(e => e.MedicineId)
                     .HasColumnName("MedicineID")
@@ -295,15 +225,9 @@ namespace UrbanMediMart.Models
                 entity.HasOne(d => d.Category)
                     .WithMany(p => p.Product)
                     .HasForeignKey(d => d.CategoryId)
-<<<<<<< HEAD
-<<<<<<< HEAD
+
                     .HasConstraintName("FK__Product__Categor__412EB0B6");
-=======
-                    .HasConstraintName("FK__Product__Categor__4222D4EF");
->>>>>>> yaman
-=======
-                    .HasConstraintName("FK__Product__Categor__4222D4EF");
->>>>>>> mohitB
+
             });
 
             OnModelCreatingPartial(modelBuilder);
--- Startup.cs
+++ Startup.cs
@@ -27,21 +27,10 @@ namespace UrbanMediMart
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-<<<<<<< HEAD
-<<<<<<< HEAD
-            services.AddDbContext<UrbanMediMartContext>(options => options.UseSqlServer(Configuration.GetConnectionString("UrbanMediMartDbConstr")));
+            services.AddDbContext<UrbanMediMartContext>(options =>
+        options.UseSqlServer(Configuration.GetConnectionString("UrbanMediMartDbConstr")));
 
-=======
-            services.AddDbContext<UrbanMediMartContext>(options =>
-        options.UseSqlServer(Configuration.GetConnectionString("UrbanMediMartDbConstr")));
->>>>>>> yaman
-=======
-            services.AddDbContext<UrbanMediMartContext>(options =>
-        options.UseSqlServer(Configuration.GetConnectionString("UrbanMediMartDbConstr")));
->>>>>>> mohitB
             services.AddControllers();
-            services.AddDbContext<UrbanMediMartContext>(options =>
-        options.UseSqlServer(Configuration.GetConnectionString("UrbanMediMartDbConstr")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
